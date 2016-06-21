27ab7c2cfd7484bdbe38aae958848b51b14fd6c5
Dean Pogni <dpogni@gmail.com>
2016-06-20 23:10:15 -0700

    New header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,21 @@
 $( document ).ready(function() {
 
+  // menu dropdown
   $('.toggle-nav').on('click', function(e) {
-    $('.menu').toggleClass("nav-open");
+    $('.header-container').toggleClass("nav-open");
     e.preventDefault();
   });
 
-
   // add/remove header-scrolled class to header on scroll
-  $(function() {
-    var header = $(".header-container");
-    var headerScrolledClass = "header-scrolled";
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll <= 0) {
-            header.removeClass(headerScrolledClass);
-        } else {
-            header.addClass(headerScrolledClass);
-        }
-    });
-});
-
+  var header = $(".header-container");
+  var headerScrolledClass = "header-scrolled";
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll <= 0) {
+          header.removeClass(headerScrolledClass);
+      } else {
+          header.addClass(headerScrolledClass);
+      }
+  });
 
 });
